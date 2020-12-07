a6a408cd1736c9466e72f59b27f5bf8fba4a5939
DESKTOP-8PLLBV6\thanh <phuhathanh247@gmail.com>
2020-12-07 22:59:12 +0700

    Username register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -27,6 +27,7 @@ class CreateNewUser implements CreatesNewUsers
 
         return User::create([
             'name' => $input['name'],
+            'username' => $input['username'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
