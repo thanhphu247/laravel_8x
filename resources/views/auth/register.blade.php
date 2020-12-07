03f0c6a44eb7183c756d051c790eaa4f6beeb31b
DESKTOP-8PLLBV6\thanh <phuhathanh247@gmail.com>
2020-12-07 22:53:37 +0700

    Username

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,6 +13,11 @@
                 <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
+            <!--username-->
+            <div>
+                <x-jet-label for="username" value="{{ __('Username') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            </div>
 
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
